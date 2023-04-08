e9cb1f827614479e4a41e51f6412b6fe625af60d
Julio Rodrigues <julio.csr.271@aluno.ufsj.edu.br>
2023-04-08 00:02:27 -0300

    feat: Added house's roof and front door

--- src/house.cpp
+++ src/house.cpp
@@ -55,61 +55,81 @@ void display(void) {
     // Edge bewtween house's right side and its front
     glBegin(GL_LINES);
         glColor3f(0.0f, 0.0f, 0.0f);
-        glVertex3f(800.0f  * COS_30, 380.0f  * SIN_30, 0.0f);
-        glVertex3f(800.0f * COS_30, 680.0f * SIN_30, 0.0f);
+        glVertex3f(800.0f * COS_30, 380.0f * SIN_30, 0.0f);
+        glVertex3f(800.0f * COS_30, 670.0f * SIN_30, 0.0f);
     glEnd();
 
     // Windows' inner contours
     glBegin(GL_LINES);
-        glVertex3f(900.0f  * COS_30, 520.0f * SIN_30, 0.0f);
-        glVertex3f(900.0f  * COS_30, 630.0f * SIN_30, 0.0f);
-        glVertex3f(1100.0f  * COS_30, 585.0f * SIN_30, 0.0f);
-        glVertex3f(1100.0f  * COS_30, 695.0f * SIN_30, 0.0f);
-
-        // glVertex3f(850.0f  * COS_30, 560.0f * SIN_30, 0.0f);
-        // glVertex3f(950.0f  * COS_30, 590.0f * SIN_30, 0.0f);
-        // glVertex3f(1050.0f  * COS_30, 625.0f * SIN_30, 0.0f);
-        // glVertex3f(1150.0f  * COS_30, 655.0f * SIN_30, 0.0f);
+        glVertex3f(900.0f * COS_30, 520.0f * SIN_30, 0.0f);
+        glVertex3f(900.0f * COS_30, 630.0f * SIN_30, 0.0f);
+        glVertex3f(1100.0f * COS_30, 585.0f * SIN_30, 0.0f);
+        glVertex3f(1100.0f * COS_30, 695.0f * SIN_30, 0.0f);
     glEnd();
 
     // Windows' contours
     glBegin(GL_LINE_LOOP);
-        glVertex3f(850.0f  * COS_30, 505.0f * SIN_30, 0.0f);
-        glVertex3f(950.0f  * COS_30, 535.0f * SIN_30, 0.0f);
-        glVertex3f(950.0f  * COS_30, 645.0f * SIN_30, 0.0f);
-        glVertex3f(850.0f  * COS_30, 615.0f * SIN_30, 0.0f);
+        glVertex3f(850.0f * COS_30, 505.0f * SIN_30, 0.0f);
+        glVertex3f(950.0f * COS_30, 535.0f * SIN_30, 0.0f);
+        glVertex3f(950.0f * COS_30, 645.0f * SIN_30, 0.0f);
+        glVertex3f(850.0f * COS_30, 615.0f * SIN_30, 0.0f);
     glEnd();
     glBegin(GL_LINE_LOOP);
-        glVertex3f(1050.0f  * COS_30, 570.0f * SIN_30, 0.0f);
-        glVertex3f(1150.0f  * COS_30, 600.0f * SIN_30, 0.0f);
-        glVertex3f(1150.0f  * COS_30, 710.0f * SIN_30, 0.0f);
-        glVertex3f(1050.0f  * COS_30, 680.0f * SIN_30, 0.0f);
+        glVertex3f(1050.0f * COS_30, 570.0f * SIN_30, 0.0f);
+        glVertex3f(1150.0f * COS_30, 600.0f * SIN_30, 0.0f);
+        glVertex3f(1150.0f * COS_30, 710.0f * SIN_30, 0.0f);
+        glVertex3f(1050.0f * COS_30, 680.0f * SIN_30, 0.0f);
     glEnd();
 
     // Windows
     glBegin(GL_QUADS);
         glColor3f(0.439216f, 0.858824f, 0.8f);
-        glVertex3f(850.0f  * COS_30, 505.0f * SIN_30, 0.0f);
-        glVertex3f(950.0f  * COS_30, 535.0f * SIN_30, 0.0f);
-        glVertex3f(950.0f  * COS_30, 645.0f * SIN_30, 0.0f);
-        glVertex3f(850.0f  * COS_30, 615.0f * SIN_30, 0.0f);
+        glVertex3f(850.0f * COS_30, 505.0f * SIN_30, 0.0f);
+        glVertex3f(950.0f * COS_30, 535.0f * SIN_30, 0.0f);
+        glVertex3f(950.0f * COS_30, 645.0f * SIN_30, 0.0f);
+        glVertex3f(850.0f * COS_30, 615.0f * SIN_30, 0.0f);
 
-        glVertex3f(1050.0f  * COS_30, 570.0f * SIN_30, 0.0f);
-        glVertex3f(1150.0f  * COS_30, 600.0f * SIN_30, 0.0f);
-        glVertex3f(1150.0f  * COS_30, 710.0f * SIN_30, 0.0f);
-        glVertex3f(1050.0f  * COS_30, 680.0f * SIN_30, 0.0f);
+        glVertex3f(1050.0f * COS_30, 570.0f * SIN_30, 0.0f);
+        glVertex3f(1150.0f * COS_30, 600.0f * SIN_30, 0.0f);
+        glVertex3f(1150.0f * COS_30, 710.0f * SIN_30, 0.0f);
+        glVertex3f(1050.0f * COS_30, 680.0f * SIN_30, 0.0f);
+    glEnd();
+
+    // Front door
+    glBegin(GL_QUADS);
+        glColor3f(0.197059f, 0.164706f, 0.164706f);
+        glVertex3f(610.0f * COS_30, 455.0f * SIN_30, 0.0f);
+        glVertex3f(610.0f * COS_30, 655.0f * SIN_30, 0.0f);
+        glColor3f(0.247059f, 0.164706f, 0.164706f);
+        glVertex3f(660.0f * COS_30, 635.0f * SIN_30, 0.0f);
+        glVertex3f(660.0f * COS_30, 435.0f * SIN_30, 0.0f);
+    glEnd();
+
+    // House's roof
+    glBegin(GL_POLYGON);
+        glColor3f(0.197059f, 0.164706f, 0.164706f);
+        glVertex3f(1100.0f * COS_60, 1000.0f * SIN_60, 0.0f);
+        glVertex3f(855.0f * COS_60, 790.0f * SIN_60, 0.0f);
+        glVertex3f(845.0f * COS_60, 800.0f * SIN_60, 0.0f);
+        glVertex3f(1100.0f * COS_60, 1020.0f * SIN_60, 0.0f);
+        glColor3f(0.347059f, 0.164706f, 0.164706f);
+        glVertex3f(1780.0f * COS_60, 1110.0f * SIN_60, 0.0f);
+        glVertex3f(1210.0f * COS_30, 770.0f * SIN_30, 0.0f);
+        glVertex3f(810.0f * COS_30, 650.0f * SIN_30, 0.0f);
     glEnd();
 
     // Right side and front of the house
     glBegin(GL_POLYGON);
         glColor3f(1.0f, 1.0f, 1.0f);
         glVertex3f(800.0f  * COS_30, 380.0f  * SIN_30, 0.0f);
-        glVertex3f(1200.0f * COS_30, 500.0f * SIN_30, 0.0f);
-        glVertex3f(1200.0f * COS_30, 800.0f * SIN_30, 0.0f);
+        glVertex3f(1190.0f * COS_30, 500.0f * SIN_30, 0.0f);
+        glVertex3f(1190.0f * COS_30, 800.0f * SIN_30, 0.0f);
         glVertex3f(800.0f * COS_30, 680.0f * SIN_30, 0.0f);
+
         glColor3f(0.5f, 0.5f, 0.5f);
-        glVertex3f(500.0f  * COS_30, 800.0f * SIN_30, 0.0f);
-        glVertex3f(500.0f  * COS_30, 500.0f * SIN_30, 0.0f);
+        glVertex3f(1100.0f * COS_60, 1000.0f * SIN_60, 0.0f);
+        glVertex3f(865.0f * COS_60, 800.0f * SIN_60, 0.0f);
+        glVertex3f(500.0f * COS_30, 500.0f * SIN_30, 0.0f);
     glEnd();
 
     glutSwapBuffers();
--- src/house.hpp
+++ src/house.hpp
@@ -5,6 +5,8 @@
 
 #define COS_30 cos(30 * M_PI / 180)
 #define SIN_30 sin(30 * M_PI / 180)
+#define COS_60 cos(60 * M_PI / 180)
+#define SIN_60 cos(60 * M_PI / 180)
 
 void reshape(int w, int h);
 void display(void);
